76a870cefba4b30683c5d65342d9a36a57b0b10d
Kio2001 <mmoottiikkiioo@gmail.com>
2022-10-03 15:54:11 +0900

    EmotionMUI完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+ 
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button
+      variant="contained"
+      sx={{
+        backgroundColor: "red",
+        "&:hover": {
+          backgroundColor: "red",
+        },
+      }}
+    >
+      ボタン
+    </Button>
   );
-}
-
+};
+ 
 export default App;
